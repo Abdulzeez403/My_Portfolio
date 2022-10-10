b1415f0382b37be5a9e45fef48004f1ea7f6046d
Abdulazeez Sodiq <70654281+Abdulzeez403@users.noreply.github.com>
2022-10-10 15:11:53 -0700

    added new feature

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 // Can also be included with a regular script tag
 
 const snipper =document.querySelector('.Skeleton');
-
 setTimeout(()=>{
   snipper.style.display="none";
 }, 2000)
-
-
-
-
-
-
 
 
 
